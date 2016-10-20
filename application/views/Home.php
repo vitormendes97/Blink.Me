57c5df9b26a7268d12052ecd04a9cc1bd2a2129a
Vitor Mendes <vitor.mendes@salaovip.com.br>
2016-10-20 18:29:14 -0200

    Topo para Desktop feita

---
+++ application/views/Home.php
@@ -0,0 +1,68 @@
+<header>
+	<!-- Topo -->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 col-xs-12" id="top01"></div>
+		</div>
+		<div class="row" id="top02" >
+			<div class="col-md-3 col-md-offset-1">
+				<img class="img-responsive" src="<?php echo base_url('/public/img/logo2.jpg') ?>">
+			</div>
+			<div class="col-md-6 col-md-offset-2" id="top03-lista">
+				<ul class="list-inline">
+					<li><a href="#">BLINK ME CLUB</a></li>
+					<li><a href="#">MANIFESTO</a></li>
+					<li><a href="#">MORUMBI TOWN</a></li>
+					<li><a href="#">CONTATO</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid pos-top">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="row">
+					<div class="col-md-6 config-border">
+						<div class="row border-dashed-01">
+							<div class="col-md-12">
+								<img class ="img-responsive img-center"  id="off_15" src="<?php echo base_url('/public/img/15_off.png'); ?> ">
+							</div>
+							</div>
+							<div class="row border-dashed-02">
+								<div class="col-md-12 ">
+									<p class="Reklame-font">Vem ai um momento único</p>
+								</div>
+							</div>
+
+							<div class="row border-dashed-02">
+								<div class="col-md-6 col-md-offset-3 col-input">
+									<input type="text" name="nome" class="form-control input-class" required="required" placeholder="Nome" style="font-family: MyRiad;font-size: 19px;">
+								</div>
+							</div>
+							<div class="row border-dashed-02">
+								<div class="col-md-6 col-md-offset-3 col-input" >
+									<input type="email" name="nome" class="form-control input-class" required="required" placeholder="Email" style="font-family: MyRiad;font-size: 19px;">
+								</div>
+								<div class="col-md-3">
+									<button type="button" style="font-family:MyRiad" class="btn btn-default btn-custom" id="btn-cadastrar">Cadastre-se</button>
+								</div>
+							</div>
+
+							<div class="row border-dashed-03">
+								<div class="col-md-5 col-md-offset-2">
+									<p id="promocao" style="font-family: MyRiad;">Promoção por tempo <u>limitado!</u></p>
+								</div>
+							</div>
+
+						</div>
+					<div class="col-md-6" id="vouncher">
+							<img id="firola" src="<?php echo base_url('/public/img/firola2.jpg'); ?>">
+					</div>
+				</div>
+	
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
---
+++ application/views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BlinkMe</title>
+	<!-- Jquery -->
+	<script   src="http://code.jquery.com/jquery-3.1.1.js"   integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="   crossorigin="anonymous"></script>
+	<!-- Bootstrap -->
+	<script type="text/javascript" src="<?php echo base_url('/public/bootstrap/js/bootstrap.js'); ?> " ></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/public/bootstrap/css/bootstrap.css'); ?> ">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/public/css/custom.css'); ?> ">
+</head>
+
+<body>
