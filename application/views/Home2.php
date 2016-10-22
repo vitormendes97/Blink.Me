90876109c7bb6f40edfb5ba2787cd5a2f1ad0d4f
Vitor Mendes <31517900@mackenzista.com.br>
2016-10-22 14:54:26 +0000

    Adicionado a função calc do css para margin

--- application/views/Home2.php
+++ application/views/Home2.php
@@ -78,14 +78,15 @@
 						
 						
 						<div class="row">
-							<div class=" col-lg-6 col-lg-offset-2 col-md-7 col-md-offset-1">
+							
+							<div class=" col-md-12 teste">
 								<input type="text" name="nome" placeholder="Nome" class="form-control input-class" id="form-nome"/>
 							</div>
 						</div>
 						
 												
 						<div class="row">
-							<div class=" col-lg-6 col-lg-offset-2 col-md-7 col-md-offset-1">
+							<div class=" col-md-12 teste ">
 								<input type="text" name="email" placeholder="Email" class="form-control input-class" id="form-email"/>
 							</div>
 						</div>
